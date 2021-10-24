1e72e8d5f5ed47340b27581a390975c11184eedc
Druue <tileyratkins@gmail.com>
2021-10-24 14:40:28 +0200

    Added endpoints for recipe retrieval / manipulation: `GET /recipes`: return a JSON list of Recipes. `POST /recipes`: create a new Recipe. `GET /recipes/:id`: get a specific Recipe. `PUT /recipes/:id`: update a specific Recipe. `DELETE /recipes/:id`: delete a specific Recipe. See handlers.rs for details.

--- server/src/handlers.rs
+++ server/src/handlers.rs
@@ -1,6 +1,20 @@
-use axum::{extract::Extension, response::IntoResponse, Json};
+use std::str::FromStr;
 
-use crate::{db_connection::DBClient, error::CheeseError, models::Recipe};
+use axum::{
+    extract::{Extension, Path},
+    response::IntoResponse,
+    Json,
+};
+use mongodb::{
+    bson::oid::ObjectId,
+    results::{DeleteResult, InsertOneResult, UpdateResult},
+};
+
+use crate::{
+    db_connection::DBClient,
+    error::CheeseError,
+    models::{Recipe, RecipeList},
+};
 
 use futures::stream::TryStreamExt;
 
@@ -8,9 +22,52 @@ pub async fn index() -> impl IntoResponse {
     "Hello World"
 }
 
-pub async fn recipes(
+pub async fn fetch_recipes(
     Extension(db_client): Extension<DBClient>,
-) -> Result<Json<Vec<Recipe>>, CheeseError> {
+) -> Result<Json<RecipeList>, CheeseError> {
     let cursor = db_client.recipe_repo.get_all_recipes().await?;
-    Ok(Json(cursor.try_collect().await?))
+    Ok(Json(RecipeList {
+        recipes: cursor.try_collect().await?,
+    }))
+}
+
+pub async fn fetch_recipe(
+    Path(id): Path<String>,
+    Extension(db_client): Extension<DBClient>,
+) -> Result<Json<Option<Recipe>>, CheeseError> {
+    let res = db_client
+        .recipe_repo
+        .read_recipe(ObjectId::from_str(&id)?)
+        .await?;
+    Ok(Json(res))
+}
+
+pub async fn push_recipe(
+    Json(recipe): Json<Recipe>,
+    Extension(db_client): Extension<DBClient>,
+) -> Result<Json<InsertOneResult>, CheeseError> {
+    let res = db_client.recipe_repo.create_recipe(recipe).await?;
+    Ok(Json(res))
+}
+
+pub async fn update_recipe(
+    Path(id): Path<String>,
+    Json(recipe): Json<Recipe>,
+    Extension(db_client): Extension<DBClient>,
+) -> Result<Json<UpdateResult>, CheeseError> {
+    let obj_id = ObjectId::from_str(&id)?;
+    let res = db_client.recipe_repo.update_recipe(obj_id, recipe).await?;
+
+    Ok(Json(res))
+}
+
+pub async fn delete_recipe(
+    Path(id): Path<String>,
+    Extension(db_client): Extension<DBClient>,
+) -> Result<Json<DeleteResult>, CheeseError> {
+    let res = db_client
+        .recipe_repo
+        .delete_recipe(ObjectId::from_str(&id)?)
+        .await?;
+    Ok(Json(res))
 }
